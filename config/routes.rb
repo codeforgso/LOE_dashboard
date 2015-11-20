26608184d3233992a05a04d961de678ab07922b8
Teddy Caddy <info@teddycaddy.com>
2015-11-19 21:39:57 -0500

    add case_histories to config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cases, only: [:show, :index]
+  resources :case_histories, only: [:show, :index]
   resources :inspections, only: [:show, :index]
   resources :violations, only: [:show, :index]
 
