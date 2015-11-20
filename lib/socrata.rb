45b4f688c44685318ba80ba5b1a01de066554c14
Teddy Caddy <info@teddycaddy.com>
2015-11-19 20:50:27 -0500

    setup pagination for Socrata data elements

--- lib/socrata.rb
+++ lib/socrata.rb
@@ -22,6 +22,41 @@ class Socrata
     get_dataset_id_by_name 'LOE Case History'
   end
 
+  def paginate(dataset_id,page)
+    # make sure page is a Fixnum, but don't let nil
+    # values get converted to zero.
+    page ||= 1
+    page = page.to_i
+
+    # array to fill with results for pagination:
+    #    * need the actual items to show on page view
+    #    * need to pad the array with nil values so it
+    #      will render pagination links
+    arr = []
+
+    # pad the previous pages with nil values
+    arr += Array.new([(page - 1) * Kaminari.config.default_per_page, 0].max)
+
+    # options for querying Socrata
+    opts = {
+      '$limit': Kaminari.config.default_per_page,
+      '$order': 'inspection_date, case_number',
+      '$offset': (page - 1) * Kaminari.config.default_per_page
+    }
+
+    # add the page being viewed to arr
+    arr += client.get(dataset_id, opts)
+
+    # if there are more pages after the current one, add more nil values
+    total_record_count = client.get(dataset_id,{'$select': 'count(*)'})[0]['count'].to_i
+    if total_record_count > page * Kaminari.config.default_per_page
+      arr += Array.new([total_record_count - (page * Kaminari.config.default_per_page), 0].max)
+    end
+
+    # paginate
+    Kaminari.paginate_array(arr).page(page)
+  end
+
   private
 
   def initialize
