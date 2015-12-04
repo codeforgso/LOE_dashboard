7e33960bf7d8797e8f6b44a977d1632e67da40fe
Teddy Caddy <info@teddycaddy.com>
2015-12-03 20:40:29 -0500

    add helper method :unique_inspection_id

--- app/helpers/inspections_helper.rb
+++ app/helpers/inspections_helper.rb
@@ -1,2 +1,8 @@
 module InspectionsHelper
+
+  def unique_inspection_id(inspection)
+    entry_date = Time.parse inspection.entry_date
+    inspection_date = Time.parse inspection.inspection_date
+    "#{inspection.case_number}*#{entry_date ? entry_date.to_i : nil}*#{inspection_date ? inspection_date.to_i : nil}"
+  end
 end
