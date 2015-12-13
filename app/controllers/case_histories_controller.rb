0cf8dcd852da2359a21a00a3d3cbbf8e9e4706e9
Teddy Caddy <info@teddycaddy.com>
2015-12-12 22:13:27 -0500

    setup linking b/t a case and its case_histories

--- app/controllers/case_histories_controller.rb
+++ app/controllers/case_histories_controller.rb
@@ -3,12 +3,22 @@ class CaseHistoriesController < ApplicationController
 
   def index
     socrata = Socrata.new
-    @case_histories = socrata.paginate(socrata.case_history_dataset_id, params[:page], {'$order': 'date, case_number'})
+    params[:filters] ||= {}
+    opts = {'$order': 'date, case_number'}
+    if params[:filters][:case]
+      opts['$where'] = "case_number = '#{params[:filters][:case]}'"
+    end
+    @case_histories = socrata.paginate(socrata.case_history_dataset_id, params[:page], opts)
   end
 
   def show
     socrata = Socrata.new
-    @case_history = socrata.client.get(socrata.case_history_dataset_id, {'$where': "case_number = '#{params[:id]}'"}).first
+    case_number = params[:id].split('*').first
+    case_history_sakey = params[:id].split('*')[1]
+    opts = {
+      '$where': "case_number = '#{case_number}' and case_history_sakey='#{case_history_sakey}'"
+    }
+    @case_history = socrata.client.get(socrata.case_history_dataset_id, opts).first
   end
 
 end
--- app/helpers/case_histories_helper.rb
+++ app/helpers/case_histories_helper.rb
@@ -1,2 +1,7 @@
 module CaseHistoriesHelper
+
+  def unique_case_history_id(case_history)
+    "#{case_history.case_number}*#{case_history.case_history_sakey}"
+  end
+
 end
