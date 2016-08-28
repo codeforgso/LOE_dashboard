90a9b20d9343ec4fd95c1bd529ec5545d7503169
Teddy Caddy <info@teddycaddy.com>
2016-08-28 15:36:26 -0400

    add helper methods for #show action

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -12,4 +12,32 @@ module CasesHelper
     end
   end
 
+  def case_items(loe_case)
+    [:case_number, :case_type, :origin, :entry_date].map do |attribute|
+      {
+        label: attribute.to_s.titleize,
+        value: loe_case.send(attribute)
+      }
+    end
+  end
+
+  def case_address_items(loe_case, opts = {})
+    if opts[:owner]
+      items = [:owner_name2, :owner_mailaddr, :owner_mailaddr2]
+      address_city = loe_case.owner_mailcity
+      address_state = loe_case.owner_mailstate
+      address_zip = loe_case.owner_mailzip
+    else
+      items = [:full_address]
+      address_city = loe_case.city
+      address_state = loe_case.state
+      address_zip = '' #loe_case.zip
+    end
+    items.map do |item|
+      loe_case[item] unless loe_case[item].blank?
+    end.compact + [
+      "#{address_city}, #{address_state} #{address_zip}"
+    ]
+  end
+
 end
--- spec/helpers/cases_helper_spec.rb
+++ spec/helpers/cases_helper_spec.rb
@@ -65,4 +65,55 @@ RSpec.describe CasesHelper, type: :helper do
     end
   end
 
+  describe 'case_items' do
+    let(:expected) do
+      [:case_number, :case_type, :origin, :entry_date].map do |attribute|
+        {
+          label: attribute.to_s.titleize,
+          value: loe_case.send(attribute)
+        }
+      end
+    end
+    let(:actual) { helper.case_items(loe_case) }
+    let(:loe_case) { create(:loe_case) }
+    it 'returns an array of items for #show template' do
+      expect(actual).to eq(expected)
+    end
+  end
+
+  describe 'case_address_items' do
+    let(:loe_case) { create(:loe_case) }
+    [false, true].each do |owner_info|
+      let(:expected) do
+        if opts[:owner]
+          items = [:owner_name2, :owner_mailaddr, :owner_mailaddr2]
+          address_city = loe_case.owner_mailcity
+          address_state = loe_case.owner_mailstate
+          address_zip = loe_case.owner_mailzip
+        else
+          items = [:full_address]
+          address_city = loe_case.city
+          address_state = loe_case.state
+          address_zip = '' #loe_case.zip
+        end
+        items.map do |item|
+          loe_case[item] unless loe_case[item].blank?
+        end.compact + [
+          "#{address_city}, #{address_state} #{address_zip}"
+        ]
+      end
+      let(:actual) { helper.case_address_items(loe_case, opts) }
+      let(:opts) do
+        if owner_info
+          {owner: true}
+        else
+          {}
+        end
+      end
+      it 'returns an array of items for #show template' do
+        expect(actual).to eq(expected)
+      end
+    end
+  end
+
 end
