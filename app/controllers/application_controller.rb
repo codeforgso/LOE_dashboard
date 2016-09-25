2b6a39847aeaba030393f388c946474b4dc293f8
Teddy Caddy <info@teddycaddy.com>
2016-09-24 22:41:03 -0400

    use AJAX to get dynamic settings for javascript

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def js_config
+    render json: {
+      google_maps_api_key: ENV['GOOGLE_MAPS_API_KEY']
+    }
+  end
 end
