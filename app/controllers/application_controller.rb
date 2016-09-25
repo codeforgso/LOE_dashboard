53f83c24978f99e8144c5301930e22a7db1bbbf2
Teddy Caddy <info@teddycaddy.com>
2016-09-24 23:16:01 -0400

    force SSL in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # require SSL for all requests
+  force_ssl if: proc { ::Rails.env.production? }
+
   def js_config
     render json: {
       google_maps_api_key: ENV['GOOGLE_MAPS_API_KEY'],
